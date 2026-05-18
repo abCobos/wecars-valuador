ae16c69b694d0541f9f593f6aaab673a747dec1e
abCobos <Ing@dexmexico.com>
2026-05-18 15:49:46 +0000

    corrige cors wix

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const OpenAI = require("openai");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
